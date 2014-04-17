72c753b12e15a3efbfcba54f79a895623671253c
zhangshanpeng <976537863@qq.com>
2014-04-17 11:02:01 +0800

    common

---
+++ ZspDBAccess/ZSQL.Common/ZSQLExtentions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZSQL.Common
+{
+    /// <summary>
+    /// ZSQL扩展方法
+    /// </summary>
+    public static class ZSQLExtentions
+    {
+        /// <summary>
+        /// 转换string为StringBuilder
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static StringBuilder ToStringBuilder(this string str)
+        {
+            return new StringBuilder(str);
+        }
+    }
+}
